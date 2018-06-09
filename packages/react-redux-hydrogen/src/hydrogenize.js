8605e2e94f858fc3eed2a0c2c1d59b7cbe32d0f6
Rick Eyre <rick.eyre@outlook.com>
2018-06-09 18:41:56 -0400

    feat(react-redux-hydrogen): add initial library

---
+++ packages/react-redux-hydrogen/src/hydrogenize.js
@@ -0,0 +1,48 @@
+import { connect } from 'react-redux';
+import compose from 'recompose/compose';
+import lifecycle from 'recompose/lifecycle';
+import isEqual from 'lodash/isEqual';
+
+import { selectors } from 'redux-hydrogen';
+
+export const hydrogenize = (
+  hydrogen,
+  method,
+  name,
+  injectProp,
+  propsToParams = () => {},
+  shouldRequest = () => true
+) => compose( // eslint-disable-line
+  connect(
+    (state, props) => {
+      const params = propsToParams(props);
+      return {
+        [injectProp]: selectors[method](state, name, params),
+        provideMeta: {
+          shouldRequest: selectors.shouldRequest(state, method, name, params) && shouldRequest(props)
+        }
+      };
+    },
+    null,
+    null,
+    {
+      areStatePropsEqual: isEqual,
+      areMergedPropsEqual: isEqual
+    }
+  ),
+  lifecycle({
+    componentWillReceiveProps(nextProps) {
+      if (
+        nextProps.provideMeta.shouldRequest &&
+        !isEqual(propsToParams(this.props), propsToParams(nextProps))
+      ) {
+        hydrogen(name)[method](propsToParams(nextProps))(nextProps.dispatch);
+      }
+    },
+    componentDidMount() {
+      if (this.props.provideMeta.shouldRequest) {
+        hydrogen(name)[method](propsToParams(this.props))(this.props.dispatch);
+      }
+    }
+  })
+);
